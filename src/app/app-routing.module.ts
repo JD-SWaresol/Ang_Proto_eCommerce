f22b4dc7afdd21e1ef1437d09af318b0d42b6e61
Juan Delgado <juan.delgado@smartwaresol.com>
2024-11-29 10:50:58 -0600

    WIP_15: Insertar Path para Product Details para cada producto mostrado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './components/signin/signin.component';
 import { SingupComponent } from './components/singup/singup.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProductoComponent } from './components/producto/producto.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'signin', component: SigninComponent},
-  {path:'signup', component:SingupComponent}
+  {path:'signup', component:SingupComponent},
+  //Insertamos el path para Producto y el Id con el que vamos a identificar la consulta
+  {path: 'producto/:id', component: ProductoComponent}
 ];
 
 @NgModule({
