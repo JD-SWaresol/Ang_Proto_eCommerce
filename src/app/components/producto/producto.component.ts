54a8ec2a3ddee91c20a63abe30c7a58e6aef1cec
Juan Delgado <juan.delgado@smartwaresol.com>
2024-11-29 16:39:54 -0600

    WIP_20: Inserta la imagen principal de los detalles de Productos

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -18,6 +18,17 @@ export class ProductoComponent implements OnInit, OnDestroy{
   producto: Producto | undefined;
   productoSub: Subscription | undefined;
 
+
+  // Galeria son imagenes que van debajo de la imagen del principal del producto
+  // - Como por ahora no se tiene nada modelado, se define la galeria como un Array de tipo 'any'
+  galeria: Array<any> = [];
+
+  // renderGaleria: Nos ayuda a definir que si se encuentra algo en la galeria entonces nos regresa 'true'
+  renderGaleria: Boolean = true;
+
+  // Creamos el elemento que usaremos para la imagen a renderizar
+  currentImg: string | undefined;
+
   // - Damos acceso a rutas asociadas sobre el componente Producto
   // - Creamos un ProductoService, para desuscribirse
   constructor(private route:ActivatedRoute, private productoService: ProductoService){
@@ -35,7 +46,11 @@ export class ProductoComponent implements OnInit, OnDestroy{
         next: (productos: Producto[]) => {
           //Se hace un filtrado para que reconozca el arreglo por el que queremos llegar principalmente
           this.producto = productos.filter(p => p.slug === this.slug )[0];
-          console.log(this.producto);
+          
+          // Traemos la imagen principal actual, que se encuentra en la posicion cero [0]
+          this.currentImg = this.producto.imageUrl[0];
+
+
         },
         error: (err: any) => {
           console.error('Error', err)
