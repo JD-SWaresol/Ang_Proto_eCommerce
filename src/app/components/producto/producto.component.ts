ce47edf7854b548c658e8acc16b2904f69a27afb
Juan Delgado <juan.delgado@smartwaresol.com>
2024-11-29 13:25:23 -0600

    WIP_18: Activated Route, obtemer paramentros de URL - Generar un OnDestroy - Especificar que necesitamos traer un Objeto de propiedades en lugar de un array de Productos

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,19 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Producto } from '../../models/producto';
+import { ProductoService } from '../../services/producto.service';
 
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.css'
 })
-export class ProductoComponent implements OnInit{
+// Implementamos un OnDestroy
+export class ProductoComponent implements OnInit, OnDestroy{
 
-  // Damos acceso a rutas asociadas sobre el componente Producto
-  constructor(private reoute:ActivatedRoute){
+  slug: string | undefined;
+
+  // Actualizamos para indicar que buscamos obtener un objeto de propiedades y no un array como tal
+  producto: Producto | undefined;
+  productoSub: Subscription | undefined;
+
+  // - Damos acceso a rutas asociadas sobre el componente Producto
+  // - Creamos un ProductoService, para desuscribirse
+  constructor(private route:ActivatedRoute, private productoService: ProductoService){
 
   }
+
+
   ngOnInit(): void {
-    // Varifica que nos trae el route
-    console.log(this.reoute);
+
+    // Traemos la propiedad slug que identifica (id) al producto que hemos seleccionado en base al ActivatedRoute con 'route'
+    this.slug = this.route.snapshot.params['id'];
+    console.log(this.slug);
+    this.productoSub = this.productoService.getProducto()
+      .subscribe( {
+        next: (productos: Producto[]) => {
+          //Se hace un filtrado para que reconozca el arreglo por el que queremos llegar principalmente
+          this.producto = productos.filter(p => p.slug === this.slug )[0];
+          console.log(this.producto);
+        },
+        error: (err: any) => {
+          console.error('Error', err)
+        }
+      }
+    )
+  }
+
+
+  ngOnDestroy(): void {
+    this.productoSub?.unsubscribe();
   }
 }
