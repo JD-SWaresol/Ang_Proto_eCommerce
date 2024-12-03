78a9ba0fbd3fd0cd1dd22e8a49e1c18ee2c9c63d
Juan Delgado <juan.delgado@smartwaresol.com>
2024-12-03 11:54:58 -0600

    WIP_30: Validacion de campos requeridos en Sign Up

--- src/app/components/singup/singup.component.ts
+++ src/app/components/singup/singup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-singup',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './singup.component.css'
 })
 export class SingupComponent {
+
+  // Traemos los elementos de Sign In que fueron colocados en su apartado TS
+
+  formSubmitted: boolean = false;
+
+  private fb = new FormBuilder;
+
+  constructor(fb:FormBuilder){
+
+  }
+
+  submittedForm: any = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(3)]],
+    confirmpassword: ['', [Validators.required, Validators.minLength(3)]]
+  })
+
+  register(){
+    
+    this.formSubmitted = true;
+
+    if (this,this.submittedForm.invalid){
+      return
+    }
+
+    console.log(this.submittedForm);
+  }
 
 }
