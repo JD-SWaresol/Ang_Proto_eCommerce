5dd1b32c92b0e1543e7809d6aa41e154dc901247
Juan Delgado <juan.delgado@smartwaresol.com>
2024-12-03 10:38:45 -0600

    WIP_28: Validacion de campos requeridos en Sign In form al ser enviados (Se muestra en Consola)

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,7 +8,32 @@ import { Component } from '@angular/core';
 })
 export class SigninComponent {
 
+  //Se usa para validar si se ha realizado el envio de datos dentro del SignIng Form => (Por default es FALSE)
+  formSubmitted: boolean = false;
+
+  private fb = new FormBuilder;
+
+  constructor(fb:FormBuilder){
+
+  }
+
+  //Este sera el 'formGroup' que sera llamado por el form del signin.html
+  loginForm: any = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(3)]]
+  })
+
   login(){
-    console.log("Envio de datos");
+    
+    //Al llamar el metodo, en automatico se cambia el estado de esta variable
+    this.formSubmitted = true;
+
+    // Validamos que los campos requeridos sean validos, de no serlo, no retorna nada
+    if (this,this.loginForm.invalid){
+      return
+    }
+
+    // Si los campos requeridos son validos, muestra la informacion en consola
+    console.log(this.loginForm);
   }
 }
