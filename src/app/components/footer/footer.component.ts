42c1da1bc68f7e5d8a080adb063559a578e9b08d
JD-SWaresol <juan.delgado@smartwaresol.com>
2024-12-31 11:00:26 -0600

    WIP_06: Prueba lista dinamica Categories en Footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  categories: { id: number, name: string, desc: string }[] = [
+    { "id": 1, "name": "Computo", "desc": "Software y Hardware" },
+    { "id": 2, "name": "Musica", "desc": "Equipos de Sonido" },
+    { "id": 3, "name": "Video Juegos", "desc": "Consolas y Streaming" },
+    { "id": 4, "name": "Libros Digitales", "desc": "Biblioteca Digital" }
+];
+
 }
