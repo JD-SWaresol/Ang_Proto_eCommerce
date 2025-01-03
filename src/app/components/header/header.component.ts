0fd0a5d661202fd25f48753d3e301b65a21f8202
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-01-03 11:58:13 -0600

    WIP_24: Agregar Cookies de Sesión dentro del TS de 'Header' Component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  //Cookie de Sesión de Prueba
+  email: string | undefined;
+
+  //Suscription para eventos de Router
+  suscription: Subscription | undefined;
+
+  constructor(private route:ActivatedRoute, private router: Router, private cookieService: CookieService){
+    // Carga los eventos de Routeo para la suscription
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.suscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        //Captura la variable de sesión
+        this.email = this.cookieService.get('name');
+        console.log('HEADER: ' + this.email);
+        this.router.navigated = false;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    //Captura la variable de sesión
+    
+  }
+
+  ngOnDestroy(): void {
+    //Cancela la suscription para los eventos de ruteo
+    if (this.suscription) {
+      this.suscription.unsubscribe();
+    }
+  }
 
 }
